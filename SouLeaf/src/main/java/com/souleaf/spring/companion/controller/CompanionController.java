c6b71f52ec44f48e9674c1de6e5f1f05fef24218
wooch <wooch@DESKTOP-EBEUDI2>
2021-05-23 20:24:14 +0900

    추가 우철

--- SouLeaf/src/main/java/com/souleaf/spring/companion/controller/CompanionController.java
+++ SouLeaf/src/main/java/com/souleaf/spring/companion/controller/CompanionController.java
@@ -2,14 +2,14 @@ package com.souleaf.spring.companion.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,41 +18,49 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.souleaf.spring.companion.main.Companion;
+import com.souleaf.spring.companion.domain.Companion;
 import com.souleaf.spring.companion.service.CompanionService;
+import com.souleaf.spring.plant.service.PlantService;
 
 //@Controller
 public class CompanionController {
 	
 //	@Autowired
 	CompanionService CompanionService;
+//	@Autowired
+	PlantService plantService;
+	
+	
+	private Logger log = LoggerFactory.getLogger(CompanionController.class);
 	
 	// 반려식물 전체 조회
 	@RequestMapping(value="CompanionList.kh", method=RequestMethod.GET)
 	public ModelAndView boardListView(ModelAndView mv) {
 		
-		ArrayList<Companion> cList = CompanionService.printAll();
-		if(!cList.isEmpty()) {
+		try {
+			ArrayList<Companion> cList = CompanionService.printAll();
 			mv.addObject("bList", cList);
 			mv.setViewName("Companion/CompanionListView");
-		}else {
-			mv.addObject("msg", "게시글 전체조회 실패");
-			mv.setViewName("common/errorPage");
+			log.info("반려식물 전체조회 성공");
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.info("반려식물 전체조회 실패");
 		}
 		return mv;
 	}
 	
 	// 반려식물 상세 조회
-	@RequestMapping(value="CompanionDetail.kh", method= {RequestMethod.GET, RequestMethod.POST})
+	@RequestMapping(value="CompanionDetail.kh", method=RequestMethod.GET)
 	public ModelAndView boardDetail(ModelAndView mv, @RequestParam("companionNo") int companionNo) {
 		
-		Companion Companion = CompanionService.printOne(companionNo);
-		if(Companion != null) {
-			// 메소드 체이닝 방식
-			mv.addObject("Companion", Companion).setViewName("Companion/CompanionDetailView");
-		}else {
-			mv.addObject("msg", "게시글 상세 조회 실패!");
-			mv.setViewName("common/errorPage");
+		try {
+			Companion Companion = CompanionService.printOne(companionNo);
+			mv.addObject("Companion", Companion);
+			mv.setViewName("Companion/CompanionDetailView");
+			log.info("반려식물 상세조회 성공");
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.info("반려식물 상세조회 실패");
 		}
 		return mv;
 	}
@@ -69,32 +77,50 @@ public class CompanionController {
 							@ModelAttribute Companion companion,
 							@RequestParam(value="uploadFile", required=false) MultipartFile uploadFile,
 							HttpServletRequest request) {
-		// 서버에 파일을 저장하는 작업
-		if(!uploadFile.getOriginalFilename().equals("")) {
-			String renameFileName = saveFile(uploadFile, request);
-			if(renameFileName != null) {
-				companion.setCompanionPickName(uploadFile.getOriginalFilename());
-				companion.setCompanionRepicName(renameFileName);
+		try {
+			// 서버에 파일을 저장하는 작업
+			if (!uploadFile.getOriginalFilename().equals("")) {
+				String renameFileName = saveFile(uploadFile, request);
+				if (renameFileName != null) {
+					companion.setCompanionPickName(uploadFile.getOriginalFilename());
+					companion.setCompanionRepicName(renameFileName);
+				}
+			} else {
+				// Plant 에서 [companion.getPlantNo]로 식물도감 검색해서 [0]번째 사진 넣기
+				// 혹은 rank함수 이용해서 파일번호가 상위인거 가져오거나 아무렇게 하세요
 			}
+//			Plant plant = plantService.printOne(companion.getPlantNo());
+//			
+//			// 캘린더 선언
+//			Calendar cal = Calendar.getInstance();
+//			// 물준날 세팅
+//			cal.setTime(companion.getCompanionLastWater());
+//			// Plant 줄주기 대입
+//			int plantWater = Integer.parseInt(plant.getPlantWater());
+//			// 물준날 + 물주기 날짜
+//			cal.add(Calendar.DATE, plantWater);
+//			// set 물 주는날 
+//			companion.setCompanionNeedWater((Date) cal.getTime());
+			
+
+			// 디비에 데이터를 저장하는 작업
+			CompanionService.registerCompanion(companion);
+			mv.setViewName("redirect:CompanionList.kh");
+			log.info("반려식물 상세조회 성공");
+		} catch (Exception e) {
+			// 실패시에 화면이동과 화면에 얼러트로 경고창 띄어주면서 리스트로 가야하는데 이거에 대한 처리가 부족함
+			// 여기서 result 로 99 든 뭐든 넘겨줘서 리절트값이 99라면 얼러트 창을 띄어주는식으로?
+			e.printStackTrace();
+			log.info("반려식물 상세조회 실패");
+			mv.setViewName("redirect:CompanionList.kh");
 		}
-		// 디비에 데이터를 저장하는 작업
-		int result = 0;
-		String path = "";
-		result = CompanionService.registerCompanion(companion);
-		if(result > 0) {
-			path = "redirect:CompanionList.kh";
-		}else {
-			mv.addObject("msg", "반려식물 등록 실패");
-			path = "common/errorPage";
-		}
-		mv.setViewName(path);
 		return mv;
 	}
 	
 	public String saveFile(MultipartFile file, HttpServletRequest request) {
 		// 파일 저장 경로 설정
 		String root = request.getSession().getServletContext().getRealPath("resources");
-		String savePath = root + "\\companionFiles";
+		String savePath = root + "/companionFiles";
 		// 저장 폴더 선택
 		File folder = new File(savePath);
 		// 폴더없으면 자동 생성
@@ -107,15 +133,13 @@ public class CompanionController {
 		String renameFileName = sdf.format(new Date(System.currentTimeMillis())) 
 								+ "." + originalFileName.substring(originalFileName.lastIndexOf(".")+1);
 								// a.bc.jpg
-		String filePath = folder + "\\" + renameFileName;
+		String filePath = folder + "/" + renameFileName;
 		// 파일저장
 		try {
 			file.transferTo(new File(filePath));
 		} catch (IllegalStateException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		// 리턴
